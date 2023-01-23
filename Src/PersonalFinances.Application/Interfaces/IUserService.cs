97725f8300d7f9f0deec0eb402d2ddc69efecc81
estevaopp <93291502+estevaopp@users.noreply.github.com>
2023-01-23 20:55:22 +0000

    IUserService

--- Src/PersonalFinances.Application/Interfaces/IUserService.cs
+++ Src/PersonalFinances.Application/Interfaces/IUserService.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PersonalFinances.Application.ViewModel.Request.User;
 
 namespace PersonalFinances.Application.Interfaces
 {
     public interface IUserService
     {
-        Task Update();
+        Task Login(LoginRequest login);
+
+        Task Create(LoginRequest login);
+
+        Task HardResetPassword(LoginRequest login);
     }
 }
