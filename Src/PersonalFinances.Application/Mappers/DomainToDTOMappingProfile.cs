726208462715f31d7f6662b2bfb1f05fad913e6e
Estevao Pereira <estevao.pereira@codenapp.com>
2023-01-25 14:45:41 -0300

    Adicionando services no escopo

--- Src/PersonalFinances.Application/Mappers/DomainToDTOMappingProfile.cs
+++ Src/PersonalFinances.Application/Mappers/DomainToDTOMappingProfile.cs
@@ -15,26 +15,15 @@ public class DomainToDTOMappingProfile : Profile
     public DomainToDTOMappingProfile()
     {
         CreateMap<User, UserResponse>();
-        CreateMap<CreateUserRequest, User>();
 
         CreateMap<UserRole, UserRoleResponse>();
-        CreateMap<CreateUserRoleRequest, UserRole>();
-        CreateMap<UpdateUserRoleRequest, UserRole>();
 
         CreateMap<RevenueCategory, RevenueCategoryResponse>();
-        CreateMap<CreateRevenueCategoryRequest, RevenueCategory>();
-        CreateMap<UpdateRevenueCategoryRequest, RevenueCategory>();
 
         CreateMap<ExpenditureCategory, ExpenditureCategoryResponse>();
-        CreateMap<CreateExpenditureCategoryRequest, ExpenditureCategory>();
-        CreateMap<UpdateExpenditureCategoryRequest, ExpenditureCategory>();
 
         CreateMap<Revenue, RevenueResponse>();
-        CreateMap<CreateRevenueRequest, Revenue>();
-        CreateMap<CreateRevenueRequest, Revenue>();
 
         CreateMap<Expenditure, ExpenditureResponse>();
-        CreateMap<CreateExpenditureRequest, Expenditure>();
-        CreateMap<UpdateExpenditureRequest, Expenditure>();
     }
 }
---
+++ Src/PersonalFinances.Infra.IoC/DependencyInjection/ServiceExtension.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using PersonalFinances.Application.Interfaces;
+using PersonalFinances.Application.Services;
+
+namespace PersonalFinances.Infra.IoC.DependencyInjection
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+
+            services.AddScoped<IUserRoleService, UserRoleService>();
+
+            services.AddScoped<IRevenueService, RevenueService>();
+
+            services.AddScoped<IRevenueCategoryService, RevenueCategoryService>();
+
+            services.AddScoped<IExpenditureService, ExpenditureService>();
+
+            services.AddScoped<IExpenditureCategoryService, ExpenditureCategoryService>();
+
+            return services;
+        }
+    }
+}
