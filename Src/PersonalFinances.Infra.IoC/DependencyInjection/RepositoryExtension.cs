d0cfccc0e81622d905988b3179afed369eeea222
Estevao Pereira <estevao.pereira@codenapp.com>
2023-01-30 14:39:28 -0300

    Add All Repositories in Scoped

--- Src/PersonalFinances.Infra.IoC/DependencyInjection/RepositoryExtension.cs
+++ Src/PersonalFinances.Infra.IoC/DependencyInjection/RepositoryExtension.cs
@@ -13,6 +13,18 @@ namespace PersonalFinances.Infra.IoC.DependencyInjection
         public static IServiceCollection AddRepository(this IServiceCollection services)
         {
             services.AddScoped(typeof(IAsyncRepository<>), typeof(AsyncRepository<>));
+
+            services.AddScoped(typeof(IExpenditureCategoryRepository), typeof(ExpenditureCategoryRepository));
+
+            services.AddScoped(typeof(IExpenditureRepository), typeof(ExpenditureRepository));
+
+            services.AddScoped(typeof(IRevenueCategoryRepository), typeof(RevenueCategoryRepository));
+
+            services.AddScoped(typeof(IRevenueRepository), typeof(RevenueRepository));
+
+            services.AddScoped(typeof(IUserRepository), typeof(UserRepository));
+
+            services.AddScoped(typeof(IUserRoleRepository), typeof(UserRoleRepository));
             
             return services;
         }
